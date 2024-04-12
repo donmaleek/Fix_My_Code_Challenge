71a31d568e2d1519e7f5ab9dccf9efc3675ee323
donmaleek <bamboloshari@gmail.com>
2024-04-12 09:46:52 -0700

    square update 2

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -29,7 +29,7 @@ class Square():
 if __name__ == "__main__":
 
     # Create an instance of the Square class with given width and height
-    s = Square(width=12, height=9)
+    s = Square(width=12, height=12)
     # Print the string representation of the square
     print(s)
     # Print the area of the square
